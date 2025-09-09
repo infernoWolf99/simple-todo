91a89720f915316090f481e5d92c11d64f4f2640
infernoWolf99 <kyeiseth58@gmail.com>
2025-09-09 19:11:42 +0000

    initial commit

---
+++ lib/providers/task_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+import '../models/task.dart';
+
+class TaskProvider extends ChangeNotifier {
+  final List<Task> _tasks = [];
+
+  List<Task> get tasks => _tasks;
+
+  void addTask(String title) {
+    _tasks.add(Task(title));
+    notifyListeners;
+  }
+
+  void removeTasks(int index) {
+    _tasks.removeAt(index);
+  }
+}
